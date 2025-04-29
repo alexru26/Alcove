a2f6c9e2dbb39a49a69d15d658748a4a1e63cf12
alexru26 <rualex2008@gmail.com>
2025-04-29 16:01:22 -0400

    Make program CLI
    Signed-off-by: alexru26 <rualex2008@gmail.com>

---
+++ src/cliHelper.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Alex Ru on 4/29/25.
+//
+
+#include "cliHelper.h"
---
+++ src/cliHelper.h
@@ -0,0 +1,16 @@
+//
+// Created by Alex Ru on 4/29/25.
+//
+
+#ifndef CLIHELPER_H
+#define CLIHELPER_H
+
+
+
+class cliHelper {
+
+};
+
+
+
+#endif //CLIHELPER_H
